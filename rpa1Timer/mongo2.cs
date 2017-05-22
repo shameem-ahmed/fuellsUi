6bb53aa99f9144e7bea549b9c0df68ab8d624b0b
Shameem Ahmed <shameem.net1@gmail.com>
2017-05-22 07:53:18 +0530

    mongoDotNet windows client app created

--- rpa1Timer/mongo2.cs
+++ rpa1Timer/mongo2.cs
@@ -24,37 +24,7 @@ namespace rpa1Timer
             _database = _client.GetDatabase(mongoDb);
         }
 
-        public async Task<Int32> userGetAll()
-        {
-            Int32 res = 0;
-
-            var collection = _database.GetCollection<BsonDocument>("users");
-
-            var filter = new BsonDocument();
-
-            //var count = 0;
-
-            using (var cursor = await collection.FindAsync(filter))
-            {
-                while (await cursor.MoveNextAsync())
-                {
-                    var batch = cursor.Current;
-
-                    foreach (var doc in batch)
-                    {
-                        Console.WriteLine(doc);
-
-                        res++;
-
-                    }
-                }
-            }
-
-            return res;
-
-        }
-
-        public async Task<List<rpa1Request>> requestGetAll()
+        public List<rpa1Request> requestGetAll()
         {
             List<rpa1Request> lstReqs = new List<rpa1Request>();
 
@@ -63,37 +33,32 @@ namespace rpa1Timer
             //var filter = new BsonDocument();
             var filter = Builders<BsonDocument>.Filter.Eq("isActive", true);
 
-            using (var cursor = await collection.FindAsync(filter))
+            var f1 = collection.Find(filter);
+            var l1 = f1.ToList<BsonDocument>();
+
+            foreach (var doc in l1)
             {
-                while (await cursor.MoveNextAsync())
-                {
-                    var batch = cursor.Current;
+                rpa1Request r1 = new rpa1Request();
 
-                    foreach (var doc in batch)
-                    {
-                        rpa1Request r1 = new rpa1Request();
+                r1.Id = doc["_id"].ToString();
+                r1.User = doc["user"].ToString();
+                r1.Time = doc["time"].ToString();
+                r1.Type = doc["type"].ToString();
+                r1.Title = doc["title"].ToString();
+                r1.Search1 = doc["search1"].ToString();
+                r1.Search2 = doc["search2"].ToString();
+                r1.Search3 = doc["search3"].ToString();
+                r1.Country = doc["country"].ToString();
+                r1.Filter = doc["filter"].ToString();
 
-                        r1.Id = doc["_id"].ToString();
-                        r1.User = doc["user"].ToString();
-                        r1.Time = doc["time"].ToString();
-                        r1.Type = doc["type"].ToString();
-                        r1.Title = doc["title"].ToString();
-                        r1.Search1 = doc["search1"].ToString();
-                        r1.Search2 = doc["search2"].ToString();
-                        r1.Search3 = doc["search3"].ToString();
-                        r1.Country = doc["country"].ToString();
-                        r1.Filter = doc["filter"].ToString();
-
-                        lstReqs.Add(r1);
-                    }
-                }
+                lstReqs.Add(r1);
             }
 
             return lstReqs;
 
         }
 
-        public async void userInsert(string email, string name, string pwd)
+        public void userInsert(string email, string name, string pwd)
         {
             var user = new BsonDocument
             {
@@ -105,9 +70,10 @@ namespace rpa1Timer
 
             var collection = _database.GetCollection<BsonDocument>("users");
 
-            await collection.InsertOneAsync(user);
+            collection.InsertOne(user);
 
         }
+
         public void CreateResponse(rpa1Request req)
         {
             
@@ -176,7 +142,7 @@ namespace rpa1Timer
             }
         }
 
-        public async void responseInsert(dynamic Data)
+        public void responseInsert(dynamic Data)
         {
             var user = new BsonDocument
             {
@@ -188,10 +154,7 @@ namespace rpa1Timer
 
             var collection = _database.GetCollection<BsonDocument>("Response");
 
-            await collection.InsertOneAsync(Data);
-
+            collection.InsertOne(Data);
         }
-
-
     }
 }
