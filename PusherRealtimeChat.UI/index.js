8b2c5b1a67e7535eabaebcaad31a4e8cd77b37ed
Shameem Ahmed <shameem.net1@gmail.com>
2017-06-30 09:55:53 +0530

    trying react

---
+++ PusherRealtimeChat.UI/index.js
@@ -0,0 +1,25 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import Pusher from "pusher-js";
+
+const baseUrl = "http://localhost:50811";
+
+const Welcome = ({ onSubmit }) => {
+    let usernameInput;
+    return (
+        <div>
+            <p>Enter your Twitter name and start chatting!</p>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                onSubmit(usernameInput.value);
+            }}>
+                <input type="text" placeholder="Enter Twitter handle here" ref={node => {
+                    usernameInput = node;
+                }}/>
+                <input type="submit" value="Join the chat" />
+            </form>
+        </div>
+    );
+};
+
